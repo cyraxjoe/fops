f46348fa314ebafdd519b9557e52dd474baf6f05
Joel Rivera <rivera@joel.mx>
2014-12-15 20:30:37 -0600

    Improve the CLI interface. The host/port are now defiend with -H/-p option flags. Add support to define the server configuration from an ini file.

--- project.clj
+++ project.clj
@@ -7,7 +7,11 @@
                  [org.apache.xmlgraphics/fop "1.0"]
                  [ring/ring-core "1.3.0"]
                  [ring/ring-jetty-adapter "1.3.0"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [com.brainbot/iniconfig "0.2.0"]
+;;                 [ring-basic-authentication "1.0.5"]
+                 ]
   :plugins [[lein-ring "0.8.7"]]
   :ring {:handler fops.web/app}
   :main ^:skip-aot fops.core
--- src/fops/core.clj
+++ src/fops/core.clj
@@ -1,13 +1,91 @@
 (ns fops.core
   (:use ring.adapter.jetty
         fops.web)
+  (:import (java.io FileNotFoundException))
   (:require [fops.web :as web])
+  (:require [clojure.string :as string])
+  (:require [com.brainbot.iniconfig :as iniconfig])
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-(def default-port "3000")
+
+(def cli-options
+  [["-p" "--port PORT" "Port number"
+    :default 3000
+    :parse-fn #(Integer/parseInt %)
+    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+   ["-H" "--host HOST" "Hostname"
+    :default "localhost"]
+   ["-c" "--config CONFIG" "Config file"]
+   ["-h" "--help"]])
+
+(def banner (str "FOPS Server v" (System/getProperty "fops.version")))
+(def section-name "fops")
+
+(defn- strip-quotes
+  "Remove the single and doble quotes from the input string."
+  [word]
+  (string/replace (string/replace word "'" "") "\"" ""))
+
+(defn- error-msg
+  "Display the msgs and on stderr one by line."
+  [& msgs]
+  (binding [*out* *err*]
+    (println (string/join "\n" (map str msgs)))))
+
+(defn- options-from-config
+  "Read the configuration file with 'ini' syntax.
+  It will look for:
+       [fops]
+         host = HOST
+         port = PORT
+  "
+  [config]
+  (try
+    (let [content (iniconfig/read-ini config)]
+      (when-let [cfg-section (get content section-name)]
+        (let [host (get cfg-section "host")
+              port (get cfg-section "port")]
+          (if (and host port)
+            {:host (->> host string/trim strip-quotes)
+             :port (Integer/parseInt port)}
+            (error-msg "Missing required variables in config file 'host'/'port'.")))))
+    (catch FileNotFoundException e (error-msg e))))
+
+
+(defn- run-server
+  "Initialize the fops server from the cmdline options."
+  [opts]
+  (if-let [options
+           (if-let [config  (get-in opts [:options :config])]
+             (options-from-config config)
+             (:options opts))]
+    (run-jetty  web/app {:port  (:port options)
+                         :host (:host options)})
+    (System/exit 1)))
+
+
+(defn- show-cli-errors-and-exit
+  "Display the cli errors on stderr and then exit with error code 1.
+  The errors  include the initial option validator."
+  [opts]
+  (error-msg  "Errors: "
+              (apply str (:errors opts))
+              banner
+              (:summary opts))
+  (System/exit 1))
+
+
+(defn- show-help
+  [opts]
+  (println banner "\n" (:summary opts)))
+
 
 (defn -main
   "Entry point of the stand alone mode of fops."
   [& args]
-  (let [port (Integer/parseInt (or (first args) default-port))]
-    (run-jetty  web/app {:port port})))
+  (let [opts (parse-opts args cli-options)]
+    (cond
+      (get-in opts [:options :help]) (show-help opts)
+      (empty? (:errors opts)) (run-server opts)
+      :else (show-cli-errors-and-exit opts))))
